78905dcf0b281c4a26f710516e15aa97e5212263
udoyen <datameshprojects@gmail.com>
2020-03-30 01:45:01 +0100

    added appsettings class file

---
+++ VggDeploy1/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VggDeploy1
+{
+    public class AppSettings
+    {
+        public string AppVersion { get; set; }
+        public string EnvironmentName { get; set; }
+    }
+}
